6ea15c27dc1701eddd02de5ca64a7c7d8d0eb0e0
NantipatSoften <keymasterviriya1150@gmail.com>
2021-03-24 17:27:11 +0700

    ⚗  Experiment new things fiel

--- pages/forms.vue
+++ pages/forms.vue
@@ -47,7 +47,15 @@
           required
         ></b-form-input>
       </b-form-group>
-
+      <b-form-group id="input-group-2" label="Image:" label-for="input-2">
+        <b-form-file
+          v-model="file1"
+          :state="Boolean(file1)"
+          placeholder="Choose a file or drop it here..."
+          drop-placeholder="Drop file here..."
+        ></b-form-file>
+        <!-- <div class="mt-3">Selected file: {{ file1 ? file1.name : "" }}</div> -->
+      </b-form-group>
       <b-button type="submit" variant="primary">Submit</b-button>
       <b-button type="reset" variant="danger">Reset</b-button>
     </b-form>
