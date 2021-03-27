af7b190c3c114f97f69b2937b38043ff21694565
NantipatSoftEn <keymasterviriya1150@gmail.com>
2021-03-27 11:57:35 +0700

    ⚗  Experiment new things$ 1

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,26 @@ export default {
     }
   },
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: [
+    [
+      "@nuxtjs/firebase",
+      {
+        config: {
+          apiKey: "AIzaSyDHi4Xzop-7s_BXgh4-HM_GkNT_6wSTSoU",
+          authDomain: "haram-nuxt.firebaseapp.com",
+          databaseURL: "https://haram-nuxt-default-rtdb.firebaseio.com",
+          projectId: "haram-nuxt",
+          storageBucket: "haram-nuxt.appspot.com",
+          messagingSenderId: "1096824363111",
+          appId: "1:1096824363111:web:7dbdae4a7c14bbcbe39cc2",
+          measurementId: "G-E90NY0GNXZ"
+        },
+        services: {
+          auth: true // Just as example. Can be any other service.
+        }
+      }
+    ]
+  ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {}
